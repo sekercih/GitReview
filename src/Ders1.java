5824ca0c476197f64bb0c3cc6e6256bf5490adda
sekercih <hkkskrc@gmail.com>
2020-09-29 20:04:40 +0300

    ilk commit1

---
+++ src/Ders1.java
@@ -0,0 +1,14 @@
+
+public class Ders1 {
+public static void main(String[] args) {
+	System.out.println("Saat 17:52");
+	
+	
+	
+	System.out.println("Saat 18:46");
+	System.out.println("Saat 19:00");
+	System.out.println("Saat 19:17-hasanbey");
+	System.out.println("Saat 19:07-conflict");
+
+}
+}
