0c484f7778e7dfac288c04ec60259575ab52d881
sekercih <hkkskrc@gmail.com>
2020-09-30 18:13:22 +0300

    2. master

--- src/Ders1.java
+++ src/Ders1.java
@@ -10,7 +10,7 @@ public static void main(String[] args) {
 	System.out.println("Saat 19:17-hasanbey");
 	System.out.println("Saat 19:07-conflict");
 	System.out.println("master");
-	
+	System.out.println("saat 18:12");
 
 }
 }
